d08a7620ea3e72b0f5b3d21a5321290a8fb8a68c
Fredrik Wollsén <fredrik@neam.se>
2016-02-12 21:50:47 +0200

    Cleaner approach to configuration + ability to trigger the code as soon as possible instead of upon a certain event

--- ng-optimizely/ng-optimizely.js
+++ ng-optimizely/ng-optimizely.js
@@ -1,9 +1,18 @@
 angular.module('ng-optimizely', ['ng'])
-.factory('optimizely', ['$rootScope', '$window', '$timeout', '$q'
+.provider('optimizely', function() {
+    var key;
+    var activationEventName = '$viewContentLoaded';
+    this.setKey = function(val) {
+        key = val;
+    };
+    this.setActivationEventName = function(val) {
+        activationEventName = val;
+    };
+    this.$get = ['$rootScope', '$window', '$timeout', '$q'
 , function($rootScope, $window, $timeout, $q) {
     var service = $window.optimizely = $window.optimizely || [];
 
-    service.loadProject = function(key, activationEventName) {
+    service.loadProject = function() {
       if (document.getElementById('optimizely-js') || key == void 0) {
         return;
       }
@@ -18,20 +27,28 @@ angular.module('ng-optimizely', ['ng'])
       script.onload = script.onreadystatechange = function () {
         deferred.resolve($window.optimizely);
       };
+      script.onerror = script.onreadystatechange = function (error) {
+        deferred.reject(error);
+      };
 
       first = document.getElementsByTagName('script')[0];
       first.parentNode.insertBefore(script, first);
 
       deferred.promise.then(function() {
-        $rootScope.$on(activationEventName || '$viewContentLoaded', function() {
-          $timeout(function() {
-            $window.optimizely.push(['activate']);
+        if (!activationEventName) {
+          console.log('pushing optimizely activate');
+          $window.optimizely.push(['activate']);
+        } else {
+          $rootScope.$on(activationEventName, function() {
+            $timeout(function() {
+              $window.optimizely.push(['activate']);
+            });
           });
-        });
+        }
       });
 
       return deferred.promise;
     };
 
     return service;
-}]);
+}]});
